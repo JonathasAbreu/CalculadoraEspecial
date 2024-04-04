9b8b2b13d07762addd076e58cbf12eff80f45955
JonathasAbreu <130936990+JonathasAbreu@users.noreply.github.com>
2024-04-04 09:53:30 -0300

    Update Divisao.cs

--- OperacoesMatematicas/Divisao.cs
+++ OperacoesMatematicas/Divisao.cs
@@ -1,5 +1,6 @@
 using Calculadora.Menus;
 using Calculadora.OperacoesMatematicas.OperacoesQuantidade;
+using Calculadora.ControleDeFluxo;
 
 namespace Calculadora.OperacoesMatematicas
 {
@@ -14,33 +15,7 @@ namespace Calculadora.OperacoesMatematicas
                 int quantidadeDivisao = int.Parse(Console.ReadLine());
                 Console.Clear();
 
-                switch (quantidadeDivisao)
-                {
-                    case 1:
-                        QuantidadeDivisao.DoisNumeros();
-                        break;
-                    case 2:
-                        QuantidadeDivisao.TresNumeros();
-                        break;
-                    case 3:
-                        QuantidadeDivisao.QuatroNumeros();
-                        break;
-                    case 4:
-                        QuantidadeDivisao.CincoNumeros();
-                        break;
-                    default:
-                        if (quantidadeDivisao == 0)
-                        {
-                            Console.WriteLine("Voltando ao menu anterior.");
-                            Thread.Sleep(3000);
-                            QuantidadeDivisao.Sair();
-                        }
-                        else
-                            Console.WriteLine("Opção inválida, escolha uma opção disponivel.\n");
-                        Thread.Sleep(2000);
-                        Dividir();
-                        break;
-                }
+                Switch.SwitchDivisao();                
             }
             catch (FormatException)
             {
